f92ff4066b6cbe73c1a7b3fc125c5bab6eca8b70
jpose <pose.jeremy@laposte.net>
2026-08-24 10:57:27 +0200

    Add files via upload

--- api/scan.js
+++ api/scan.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Méthode non autorisée' });
   }
@@ -40,4 +40,4 @@ export default async function handler(req, res) {
   } catch (err) {
     return res.status(500).json({ error: err.message });
   }
-}
+};
