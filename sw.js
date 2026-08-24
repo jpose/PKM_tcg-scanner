5f260e0823c65fcbab016bbfc109117a4d8daaa9
jpose <pose.jeremy@laposte.net>
2026-08-24 09:32:11 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,6 +3,5 @@ self.addEventListener('install', (e) => {
 });
 
 self.addEventListener('fetch', (e) => {
-  // Permet au navigateur de gérer les requêtes réseau normalement
   e.respondWith(fetch(e.request));
 });
