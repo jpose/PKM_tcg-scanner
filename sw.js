6eef3ce2e40cd46bce668181c26f26ee6c54a323
jpose <pose.jeremy@laposte.net>
2026-08-24 11:03:21 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,16 @@
-self.addEventListener('install', (e) => {
+self.addEventListener('install', () => {
   self.skipWaiting();
 });
 
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
 self.addEventListener('fetch', (e) => {
+  // NE PAS intercepter les appels API, les laisser passer en réseau direct
+  if (e.request.url.includes('/api/')) {
+    return;
+  }
+  
   e.respondWith(fetch(e.request));
 });
